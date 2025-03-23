7aab523e0415329e21edaeea610e90bbd61d973e
MICHAEL LIMACO <limaco.michael@gmail.com>
2025-03-22 21:09:09 -0500

    update historia 02

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -4,6 +4,7 @@ import { Link, useOutletContext } from 'react-router-dom';
 const ContactList = () => {
   const context = useOutletContext();
   const [searchTerm, setSearchTerm] = useState('');
+  const [sortOrder, setSortOrder] = useState('asc');
 
   if (!context) {
     return <p>No hay contactos disponibles</p>;
@@ -20,6 +21,18 @@ const ContactList = () => {
     contact.fullname.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const sortedContacts = filteredContacts.sort((a, b) => {
+    if (sortOrder === 'asc') {
+      return a.fullname.localeCompare(b.fullname);
+    } else {
+      return b.fullname.localeCompare(a.fullname);
+    }
+  });
+
+  const handleSortOrderChange = () => {
+    setSortOrder(prevOrder => (prevOrder === 'asc' ? 'desc' : 'asc'));
+  };
+
   return (
     <div>
       <h2>Lista de Contactos</h2>
@@ -29,8 +42,11 @@ const ContactList = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       /> {/* Campo de búsqueda */}
+      <button onClick={handleSortOrderChange}>
+        Ordenar {sortOrder === 'asc' ? 'Descendente' : 'Ascendente'}
+      </button> {/* Botón para ordenar contactos */}
       <ul>
-        {filteredContacts.map(contact => (
+        {sortedContacts.map(contact => (
           <li key={contact.id}>
             <Link to={`contact/${contact.id}`} onClick={() => onSelectContact(contact)}>
               {contact.fullname}
